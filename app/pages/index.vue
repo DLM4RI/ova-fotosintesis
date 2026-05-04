705db311fe271b7293421214bd3430ad2e85988c
DLM4RI <mario.andres.castro.9.4@gmail.com>
2026-05-03 21:34:20 -0500

    Upload V.0.0.6 Pre-Alpha

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,7 +11,8 @@
         class="mx-auto"
       />
       <v-card-title class="text-h5 mb-2 font-weight-black">
-        Fotosíntesis en las plantas
+        Fotosíntesis <br />
+        en las plantas
       </v-card-title>
 
       <v-card-subtitle class="mb-4">
@@ -47,29 +48,71 @@
 
       <v-divider class="mb-4" />
 
-      <p class="text-overline font-weight-black mb-3">Creado por</p>
+      <p class="text-overline font-weight-black">Creado por</p>
 
       <v-list class="text-left">
-        <v-list-item title="Mario Castro" subtitle="Desarrollador/Programador">
+        <!-- YO -->
+
+        <v-list-item title="Mario Castro" subtitle="Desarrollador">
           <template #prepend>
             <v-avatar size="60">
               <v-img :src="useAsset('mario.png')" />
             </v-avatar>
           </template>
+          <template #append>
+            <v-avatar size="60">
+              <CodeAlt fill="#ff8600" />
+            </v-avatar>
+          </template>
         </v-list-item>
-        <v-list-item
-          title="Rosendo De Castro"
-          subtitle="Desarrollador/Programador"
-        >
+
+        <!-- Rosendo -->
+
+        <v-list-item title="Rosendo De Castro" subtitle="Desarrollador">
           <template #prepend>
             <v-avatar size="60">
               <v-img :src="useAsset('rosendo.jpeg')" />
             </v-avatar>
           </template>
+          <template #append>
+            <v-avatar size="60">
+              <CodeAlt fill="#ff8600" />
+            </v-avatar>
+          </template>
+        </v-list-item>
+
+        <!-- Fabio -->
+
+        <v-list-item title="Fabio Cantero" subtitle="QA Engineer">
+          <template #prepend>
+            <v-avatar size="60">
+              <v-img :src="useAsset('fabio.png')" />
+            </v-avatar>
+          </template>
+          <template #append>
+            <v-avatar size="60">
+              <ListUlSquare fill="#006aff" />
+            </v-avatar>
+          </template>
+        </v-list-item>
+
+        <!-- Andrea -->
+
+        <v-list-item title="Andrea Hernandez" subtitle="Project Manager">
+          <template #prepend>
+            <v-avatar size="60">
+              <v-img :src="useAsset('andrea.png')" />
+            </v-avatar>
+          </template>
+          <template #append>
+            <v-avatar size="60">
+              <Briefcase fill="#ff40e3" />
+            </v-avatar>
+          </template>
         </v-list-item>
       </v-list>
 
-      <v-divider class="mb-4" />
+      <v-divider class="mb-4 mt-4" />
 
       <v-btn color="green" rounded="pill" size="large" to="/mantenimiento">
         Comenzar
@@ -81,9 +124,9 @@
 </template>
 
 <script setup>
-import { LeafAlt } from "@boxicons/vue"
+import { LeafAlt, CodeAlt, ListUlSquare,Briefcase } from "@boxicons/vue";
 
 definePageMeta({
   layout: "default",
-})
+});
 </script>
